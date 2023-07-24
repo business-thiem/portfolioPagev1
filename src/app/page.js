690eeed5a508c7a128c54af189e168eabea932ed
Thiem <thiem.huynh0@gmail.com>
2023-07-24 16:05:50 -0400

    update

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,8 @@ import web3 from "../../public/web3.png";
 import web4 from "../../public/web4.png";
 import web5 from "../../public/web5.png";
 import web6 from "../../public/web6.png";
+
+//TODO fix useState
 import { useState } from "react";
 
 export default function Home() {
