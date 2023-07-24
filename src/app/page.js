b22d4494cc938882f1557ed7c8ac0f499e0f450e
Thiem <thiem.huynh0@gmail.com>
2023-07-24 16:05:08 -0400

    update, night mode doesn't work yet bc useState

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,21 @@
-import Image from 'next/head';
+import Head from 'next/head';
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube} from 'react-icons/ai';
+import Image from "next/image";
+import myPortrait from "../../public/thiem-pro-photo.jpg";
+import design from "../../public/design.png";
+import code from "../../public/code.png";
+import consulting from "../../public/consulting.png";
+import web1 from "../../public/web1.png";
+import web2 from "../../public/web2.png";
+import web3 from "../../public/web3.png";
+import web4 from "../../public/web4.png";
+import web5 from "../../public/web5.png";
+import web6 from "../../public/web6.png";
+import { useState } from "react";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
     <div>
       <Head>
@@ -9,10 +24,106 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <section className="bg-blue-700 min-h-screen">
-          <h1>hello</h1>
+      <main class="bg-white px-10 md:px-20 lg:px-40">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+          <h1 className="text-xl font-burtons">Thiem Huynh - Software Developer</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
+              </li>
+              <li>
+                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">Thiem Huynh</h2>
+            <h3 className="text-2xl py-2 md:text-3xl">Self Motivation is cool</h3>
+            <p className="text-md py-5 leadin-8 text-gray-800 md:text-xl max-w-3xl mx-auto">
+              Follow the life of an Army soldier who teaches himself how to code. My <span className="text-teal-500"> <a href="https://www.dvidshub.net/portfolio/1719421/thiem-huynh" target="_blank">military career</a></span> has taken me to many amazing places and I've met even more amazing people, but it's time to trade in the rifle for a cup of coffee and a keyboard.
+            </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <AiFillTwitterCircle/>
+            <AiFillLinkedin />
+            <AiFillYoutube />
+          </div>
+          <div className="relative mt-20 overflow-hidden md:h-96 md:w-96 mx-auto" layout="fill" objectFit='cover'>
+            <Image src={myPortrait} className="rounded-full w-80 h-80 mx-auto" />
+          </div>
         </section>
+
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">My Projects</h3>
+            <p className="text-md py-2 leading-8 text-gray-80">
+              I've always enjoyed good puzzle games and Software Engineering has scratched that itch. Here are some web apps that I've made for fun. 
+            </p>
+          </div>
+
+          <div className="lg:flex gap-10">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image src={design} width={100} height={100} className="mx-auto" />
+              <h3 className="text-lg font-medium pt-8 pb-2">Designs</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <h4 className="py-4 text-teal-600">Tools I used</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Lightroom</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Premiere</p>
+              <p className="text-gray-800 py-1">After Effects</p>
+            </div>
+
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image src={code} width={100} height={100} className="mx-auto" />
+              <h3 className="text-lg font-medium pt-8 pb-2">Designs</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <h4 className="py-4 text-teal-600">Tools I used</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Lightroom</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Premiere</p>
+              <p className="text-gray-800 py-1">After Effects</p>
+            </div>
+
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image src={consulting} width={100} height={100} className="mx-auto" />
+              <h3 className="text-lg font-medium pt-8 pb-2">Designs</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <h4 className="py-4 text-teal-600">Tools I used</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Lightroom</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Premiere</p>
+              <p className="text-gray-800 py-1">After Effects</p>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Portfolio</h3>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper sit amet risus nullam eget felis eget nunc. Eu nisl nunc mi ipsum faucibus. Neque gravida in fermentum et sollicitudin.
+          </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1"><Image src={web1} className="rounded-lg object-cover" width={'100%'} height={'100%'}/></div>
+            <div className="basis-1/3 flex-1"><Image src={web2} className="rounded-lg object-cover" width={'100%'} height={'100%'}/></div>
+            <div className="basis-1/3 flex-1"><Image src={web3} className="rounded-lg object-cover" width={'100%'} height={'100%'}/></div>
+            <div className="basis-1/3 flex-1"><Image src={web4} className="rounded-lg object-cover" width={'100%'} height={'100%'}/></div>
+            <div className="basis-1/3 flex-1"><Image src={web5} className="rounded-lg object-cover" width={'100%'} height={'100%'}/></div>
+            <div className="basis-1/3 flex-1"><Image src={web6} className="rounded-lg object-cover" width={'100%'} height={'100%'}/></div>
+          </div>
+        </section>
+
       </main>
     </div>
   );
